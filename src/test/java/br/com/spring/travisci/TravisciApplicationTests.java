64af08c4391e9ef6d9e4cc304fbdc4fa925a8ded
Jose Robson <joserobsonsiqueira23@hotmail.com>
2023-09-10 18:53:15 -0300

    Hello from Travis CI

---
+++ src/test/java/br/com/spring/travisci/TravisciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.travisci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
